28948dd2b15bbf07f7352ecd6f9cec4d1d1a584c
Nobu Chen <nobu1520@icloud.com>
2017-11-13 16:47:10 +0100

    ajust

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const request = require('request');
 
 http.createServer((req, res) => {
   //res.setHeader('Host', 'google.fr');
-  const website = 'google.fr';
-  const target = req.url === '/' ? website : `${website}/${req.url}`;
-  res.setHeader('Content-Type', 'text/html;charset=utf8');
+  //const website = 'google.fr';
+  //const target = req.url === '/' ? website : `${website}/${req.url}`;
+  //res.setHeader('Content-Type', 'text/html;charset=utf8');
 
-  console.log(`targetting ${target}`);
+  //console.log(`targetting ${target}`);
 
-  request(`http://www.${target}`).pipe(res);
+  //request(`http://www.${target}`).pipe(res);
+  res.end('hello');
 }).listen(9999);
