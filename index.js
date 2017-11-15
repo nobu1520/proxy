caa6486172761c10f734057fe78a46282fd6e0a3
Nobu Chen <nobu1520@icloud.com>
2017-11-15 14:44:38 +0100

    fixed url and running errors

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 const http = require('http');
 const request = require('request');
+const port = process.env.PORT || 9999;
+const defaultWebsite = 'google.fr';
+let currentWebsite = defaultWebsite;
 
 http.createServer((req, res) => {
   //res.setHeader('Host', 'google.fr');
-  //const website = 'google.fr';
-  //const target = req.url === '/' ? website : `${website}/${req.url}`;
-  //res.setHeader('Content-Type', 'text/html;charset=utf8');
+  const splittedUrl = req.url.split('/foxy/');
+  const websiteOptionFromUrl = splittedUrl[splittedUrl.length - 1];
+  
+  if (websiteOptionFromUrl !== req.url) {
+    currentWebsite = websiteOptionFromUrl;
+  } else {
+    currentWebsite = defaultWebsite;
+  }
 
-  //console.log(`targetting ${target}`);
+  const target = req.url === '/' || req.url.startsWith('/foxy/') ? currentWebsite : `${currentWebsite}${req.url}`;
 
-  //request(`http://www.${target}`).pipe(res);
-  res.end('hello');
-}).listen(9999);
+  res.setHeader('Content-Type', 'text/html;charset=utf8');
+  res.statusCode = 200;
+
+  console.log(`targetting ${target}`);
+
+  req.on('error', (err) => {
+    res.statusCode = 500;
+    res.end(`An error occured : ${err}`);
+  });
+
+  res.on('error', (err) => {
+    res.statusCode = 500;
+    res.end(`An error occured : ${err}`);
+  });
+
+  request(`http://www.${target}`)
+    .on('error', (err) => {
+      currentWebsite = defaultWebsite;
+      res.statusCode = 500;
+      res.end(`<!DOCTYPE html><html><head><link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'><title>Target not found</title><style>html, body { margin: 0; padding: 0; height: 100%; display: flex; flex-flow: column wrap; justify-content: center; align-items: center; background: url('https://www.technobuffalo.com/wp-content/uploads/2013/07/mad-max-official-img4.jpg') no-repeat center; background-size: cover; font-family: 'Roboto', sans-serif; color: black; }</style></head><body><p>Target '${target}' cannot be found...</p></body></html>`);
+    })
+    .pipe(res);
+}).listen(port);
